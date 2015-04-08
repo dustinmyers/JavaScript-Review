30e495f0b19c2c8740cd6bc20215b8e8edf56225
Dustin Myers <dustint314@gmail.com>
2015-04-08 15:29:48 -0600

    finished through first = second problem

--- arrayReview.js
+++ arrayReview.js
@@ -16,7 +16,6 @@ alert(last(threeItems));
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
 var looper = function(arr) {
-	debugger;
 	for(var i = 0; i < arr.length; i++) {
 		if (arr[i] % 2  !== 0) {
 			arr.splice(i, 1);
@@ -33,24 +32,42 @@ looper(evenArray);
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. 
+//Your job is to write a function that will get a random number, 
+//then loop through the array to see if that random number is in the array. 
+//If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
-}
+};
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+var include = function(arr, search) {
+	debugger;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr.indexOf(search) >= 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+
+include(randomArray, getRandomArbitrary());
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+//Create a copy of first and save it into second. Then, add 6 and 7 
+//to the end of second. When you run this, first should be just [1,2,3,4,5] 
+//and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
 var second;
 
-  //code here
+second = first.slice(0);
+second.push(6,7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
